c2fd2ab4f03a344f5b6249f03655fa09efeea2ea
ElKamelHK <hamdiek58@gmail.com>
2021-08-04 15:42:50 +0100

    initialisation et configuration de projet

---
+++ src/test/java/com/usermanger/ecole/EcoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usermanger.ecole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
